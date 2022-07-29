696782f07b1cf9961382d578dc88119e8bd38c03
idvele <ilari.katajamaki1@gmail.com>
2022-07-29 14:00:40 +0300

    commit w7

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function Helper()
+{
+    window.alert("tämä toimii!")
+}
+
+function colorOn(id) {
+
+    document.getElementById(id).style.backgroundColor = 'red';
+}
+
+function colorOff(id){
+
+    document.getElementById(id).style.backgroundColor = 'cadetblue';
+}
+
+function colorOnC(Class) {
+    document.getElementByClass(Class).style.backgroundColor = 'red';
+}
+
+function red(object) {
+    object.style.background = 'red'
+}
+
+function white(object) {
+    object.style.background = 'white'
+}
