863b8d46d5d98b06a6a9ee1190f8ee0e40878f41
idvele <ilari.katajamaki1@gmail.com>
2022-08-05 16:17:57 +0300

    joke api valmis, search poistettu

---
+++ Models/Joke.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningDiary_IK0._1.Models
+{
+    public class Joke
+    {
+        public string type { get; set; }
+        public string joke { get; set; }
+        public string setup { get; set; }
+        public string delivery { get; set; }
+        public int id { get; set; }
+        public Flags flags { get; set; }
+    }
+
+
+    public class Flags
+    {
+        public bool nsfw { get; set; }
+        public bool religious { get; set; }
+        public bool political { get; set; }
+        public bool racist { get; set; }
+        public bool sexist { get; set; }
+
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,7 +5,7 @@
 
 function Helper()
 {
-    window.alert("tämä toimii!")
+    window.alert("pls send help")
 }
 
 function colorOn(id) {
@@ -22,10 +22,63 @@ function colorOnC(Class) {
     document.getElementByClass(Class).style.backgroundColor = 'red';
 }
 
-function red(object) {
-    object.style.background = 'red'
-}
+//function red(object) {
+//    object.style.background = 'red'
+//}
 
-function white(object) {
-    object.style.background = 'white'
-}
+//function white(object) {
+//    object.style.background = 'white'
+//}
+
+$(document).ready(function () {
+    $('.form-control').bind("mouseover", function () {
+        var color = $(this).css("background-color");
+
+        $(this).css("background", "#F9C3F3");
+
+        $(this).bind("mouseout", function () {
+            $(this).css("background", color);
+        })
+    })
+})
+
+$(document).ready(function () {
+    $('.JokeBtn').bind("click", function () {  //tähän targetti
+        var jokeApi = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist";
+
+        $.getJSON(jokeApi, function (data) {
+            console.log(data.joke)
+            console.log(data.setup)
+            console.log(data.delivery)
+
+           
+            if (data.joke != undefined) {
+                document.getElementById('jokePrint').innerHTML =
+                    data.joke
+
+            }
+            else {
+            
+                function Delivery(del) {
+                    document.getElementById('jokePrint').innerHTML =
+                        data.setup + "<br/>" + data.delivery
+                }
+                setTimeout(Delivery, 4000);
+                document.getElementById('jokePrint').innerHTML =
+                    data.setup;
+
+                
+            }
+
+        })
+    })
+})
+
+//Aloita aina alla olevalla perus JQuery -käskyllä JA lisää toiminnot funktion { } sisään.
+//JQuery käskyissä AINA ensin valinta (mihin targetoidaan) ja sitten JQuery event (mitä sille tehdään)
+$(document).ready(function () {
+    $("#test").click(function () {
+        $("#test-message").toggle();
+    });
+});
+
